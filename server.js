b75adc3823a30a29a02f035fe29d8d841619cd60
Edison Garcia Chinas <edisga@microsoft.com>
2018-10-29 11:36:06 -0500

    Updating

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
 var port = process.env.port || 3000
-app.get('/', function (req, res) {
-    res.send(process.version)
-  });
-
+app.get('/', function (req, res) { res.send("Process Version: " + process.version)})
 app.listen(port, () => console.log('Example app listening on port 3000!'))
